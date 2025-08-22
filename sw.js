e3eeb83176788d02ecffcaf362f1803da79e1f5f
Alvkrys <camoapa.2906@gmail.com>
2025-08-22 13:33:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'talalog-v3';  // <- SUBIDO (antes v2)
+const CACHE_NAME = 'talalog-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -8,23 +8,24 @@ const ASSETS = [
   './manifest.webmanifest'
 ];
 
-self.addEventListener('install', e=>{
+self.addEventListener('install', (e)=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(
-    keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-  )));
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))
+  );
+  self.clients.claim();
 });
-self.addEventListener('fetch', e=>{
-  if(e.request.method!=='GET') return;
+self.addEventListener('fetch', (e)=>{
+  const { request } = e;
+  if (request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached=>{
-      return cached || fetch(e.request).then(res=>{
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(c=>c.put(e.request, clone));
-        return res;
-      }).catch(()=> cached);
-    })
+    caches.match(request).then(res => res || fetch(request).then(net => {
+      const copy = net.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(request, copy));
+      return net;
+    }).catch(()=>res))
   );
 });
